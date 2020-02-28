f493feebd766e7419b0dd04ae6b10c96aa8e3875
LizandroV <61571830+LizandroV@users.noreply.github.com>
2020-02-27 23:37:20 -0500

    Update ScreenShot2.py

--- ScreenShot2.py
+++ ScreenShot2.py
@@ -9,3 +9,19 @@ del mem  # Release bitmap
 bmp.SaveFile('screenshot.png', wx.BITMAP_TYPE_PNG)
 
 print("Test")
+
+1.- cd "ruta de la carpeta"
+2.- git init
+3.- git status
+4.- git add -A
+5.- git status
+6.- git commit -m "Version#"
+7.- git remote add origin %link_git%
+8.- git push origin mater
+
+--------------------------------
+
+1.- git add -A
+2.- git status
+3.- git commit -m "Version#"
+4.- git push origin master
