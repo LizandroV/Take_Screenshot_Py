bf8b7809959eb6469fe0ae231b83ce22905d59c0
LizandroV <61571830+LizandroV@users.noreply.github.com>
2020-02-29 00:36:36 -0500

    GIT
    Como subir al git

--- ScreenShot2.py
+++ ScreenShot2.py
@@ -1,14 +1,12 @@
 1.- cd "ruta de la carpeta"
 2.- git init
-3.- git status
 4.- git add -A
 5.- git status
 6.- git commit -m "Version#"
 7.- git remote add origin %link_git%
-8.- git push origin mater
+8.- git push origin master
 
---------------------------------
-
+---------AGREGAR------------
 1.- git add -A
 2.- git status
 3.- git commit -m "Version#"
