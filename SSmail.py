61bced4c2c03b6e92f553ac118799f61ad313090
LizandroV <61571830+LizandroV@users.noreply.github.com>
2020-03-01 17:51:20 -0500

    Version 3.0
    Realiza 2 capturas de pantalla y lo envia por correo.
    
    Nota: Las variables estan en el codigo.

--- SSmail.py
+++ SSmail.py
@@ -3,35 +3,31 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.encoders import encode_base64
-
-
-#VARIABLES
-msjto="lizandro_131@hotmail.com"
-msjcc=""
-msjsubject=StringVar()
-nameshow=StringVar()
-filename=StringVar()
-nameshow.set("Seleccionar Archivo...")
+import pyautogui
+import time
 
 #CUENTA CORREO
 user = "lizandrovivanco16@gmail.com"
 pasw = "lizandrito232114"
 de = "Lizandro V"
 
+#VARIABLES
+dest="lizandro_131@hotmail.com"
+copia="lizandrovivanco16@gmail.com"
+asunto="Error Portal"
+textomsj="Estimados, se encontro el siguiente error"
+name1="Error 1"
+name2="Error 2"
+archivo1="C:\\Users\\No Tocar!!\\Downloads\\Capturas\\Error 1.png"
+archivo2="C:\\Users\\No Tocar!!\\Downloads\\Capturas\\Error 2.png"
 
 def enviarmsj():
-    dest =  msjto.get()
-    copia = msjcc.get()
-    asunto =  msjsubject.get()
-    mensaje =   mesagetext.get("1.0", END)
-    archivo = filename.get()
-
     gmail = smtplib.SMTP("smtp.gmail.com",587)
     gmail.starttls()
     try:
+        print("iniciar")
         gmail.login(user,pasw)
         gmail.set_debuglevel(1)
-
         rcpt = copia.split(",") + [dest]
 
         header = MIMEMultipart()
@@ -40,36 +36,39 @@ def enviarmsj():
         header['To'] = dest
         header['Cc'] = copia
     
-        mensaje = MIMEText(mensaje, 'html')
+        mensaje = MIMEText(textomsj, 'html')
         header.attach(mensaje)
+        
+        print("adjuntar")
+        if (os.path.isfile(archivo1)):
+            adjunto1 = MIMEBase('application', 'octet-stream')
+            adjunto1.set_payload(open(archivo1, "rb").read())
+            encode_base64(adjunto1)
+            adjunto1.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(archivo1))
+            header.attach(adjunto1)
 
-        if (os.path.isfile(archivo)):
-            adjunto = MIMEBase('application', 'octet-stream')
-            adjunto.set_payload(open(archivo, "rb").read())
-            encode_base64(adjunto)
-            adjunto.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(archivo))
-            header.attach(adjunto)
+        if (os.path.isfile(archivo2)):
+            adjunto2 = MIMEBase('application', 'octet-stream')
+            adjunto2.set_payload(open(archivo2, "rb").read())
+            encode_base64(adjunto2)
+            adjunto2.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(archivo2))
+            header.attach(adjunto2)
 
         gmail.sendmail(de,rcpt,header.as_string())
 
+    except Exception as e:
+        print(e)
+
+    finally:
         gmail.quit()
+        print("Conexion cerrada")
 
-        messagebox.showinfo("Email", "Correo enviado con exito")
+def screenshot():
+    time.sleep(5)
+    pyautogui.screenshot("C:\\Users\\No Tocar!!\\Downloads\\Capturas\\" + str(name1) + ".png")
+    time.sleep(5)
+    pyautogui.screenshot("C:\\Users\\No Tocar!!\\Downloads\\Capturas\\" + str(name2) + ".png")
+    time.sleep(5)
 
-    except:
-        messagebox.showwarning("Atencion", "Error de envio")
-        gmail.quit()
-
-
-def adjuntararchivo():
-    global namefile
-    namefile =  filedialog.askopenfilename(title = "Seleccionar Archivo...",filetypes = (("Archivos Excel","*.xlsx"),("Todos los Archivos","*.*")))
-    try:
-        if namefile == "":
-            nameshow.set("Seleccionar Archivo...")
-        else:
-            nameshow.set(os.path.basename(namefile))
-            filename.set(namefile)
-    except:
-        nameshow.set("Seleccionar Archivo...")
-
+screenshot()
+enviarmsj()
--- ScreenShots.py
+++ ScreenShots.py
@@ -1,10 +1,25 @@
 import pyautogui
 import time
+import os
+import glob
 
-x=1
+name1="Error 1"
+name2="Error 2"
 
-while x<4:
-    pyautogui.screenshot("C:\\Users\\No Tocar!!\\Downloads\\image_" + str(x) + ".png")
-    x+=1
-    time.sleep(2)
+def screenshot():
+    time.sleep(5)
+    pyautogui.screenshot("C:\\Users\\No Tocar!!\\Downloads\\Capturas\\" + str(name1) + ".png")
+    time.sleep(5)
+    pyautogui.screenshot("C:\\Users\\No Tocar!!\\Downloads\\Capturas\\" + str(name2) + ".png")
 
+screenshot()
+
+print("ver Archivos")
+files = "C:\\Users\\No Tocar!!\\Downloads\\Capturas"
+for the_file in os.listdir(files): 
+    file_path = os.path.join(files, the_file)
+    try: 
+        if os.path.isfile(file_path): 
+            print(file_path) 
+    except Exception as e: 
+        print (e)
